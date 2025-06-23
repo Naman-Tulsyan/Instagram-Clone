3e09054fe2f3302d8b36ef9d6a605b333be1c394
Naman Tulsyan <namantulsyan976@gmail.com>
2025-06-23 13:26:50 +0530

    change config file

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://instagram-clone-nigm.onrender.com/api/v1";
+export const API_URL = "http://localhost:8080/api/v1";
 export const USER_API_URL = `${API_URL}/user`;
 export const MESSAGE_API_URL = `${API_URL}/message`;
 export const POST_API_URL = `${API_URL}/post`;
